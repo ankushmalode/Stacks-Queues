31d11fbefe0ca86c8ee86ae1f532db5a0b4d7b1f
ankushmalode <you@example.com>
2021-10-12 20:09:56 +0530

    Stacks and Queues problem

---
+++ src/main/java/com/bridgelabz/StacksAndQueues/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.StacksAndQueues;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
